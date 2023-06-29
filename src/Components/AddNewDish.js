f55b84b5906ff5a60f324b9218adc00549074494
Animesh Dubey <Animesh.Dubey@KornFerry.com>
2023-06-29 12:10:43 +0530

    validation added

--- src/Components/AddNewDish.js
+++ src/Components/AddNewDish.js
@@ -11,17 +11,34 @@ const AddNewDish = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+    // if (!dishName || !price || !weight) {
+    //   alert('Please fill in all fields.');
+    //   return;
+    // }
+
+    // if (parseFloat(price) < 0 || parseFloat(price) > 2000) {
+    //   alert('Price must be in the range of 0 to 2000.');
+    //   return;
+    // }
+
+    // if (parseFloat(weight) < 0 || parseFloat(weight) > 999.99) {
+    //   alert('Weight must be in the range of 0 to 999.99.');
+    //   return;
+    // }
+
     // Create a new dish object
     const newDish = {
       dishName,
       price,
-      weight
+      weight,
     };
 
     try {
       // Send the new dish data to the backend API
-      console.log(newDish);
-      const response = await axios.post('https://webappnewcreated.azurewebsites.net/api/v1', newDish);
+      const response = await axios.post(
+        'https://webappnewcreated.azurewebsites.net/api/v1',
+        newDish
+      );
 
       // Reset the form fields
       setdishName('');
@@ -34,48 +51,51 @@ const AddNewDish = () => {
   };
 
   return (
-
     <div className="container my-5">
       <form onSubmit={handleSubmit}>
-      <div className="form-group">
-        <label htmlFor="dishName">Name of Dish:</label>
-        <input
-          type="text"
-          id="dishName"
-          value={dishName}
-          onChange={(e) => setdishName(e.target.value)}
-          className="form-control"
-          required
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="price">Price:</label>
-        <input
-          type="number"
-          id="price"
-          value={price}
-          onChange={(e) => setPrice(e.target.value)}
-          className="form-control"
-          required
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="weight">Weight:</label>
-        <input
-          type="text"
-          id="weight"
-          value={weight}
-          onChange={(e) => setWeight(e.target.value)}
-          className="form-control"
-          required
-        />
-      </div>
-      <button type="submit" className="btn btn-primary">
-        Add Dish
-      </button>
-    </form>
+        <div className="form-group">
+          <label htmlFor="dishName">Name of Dish:</label>
+          <input
+            type="text"
+            id="dishName"
+            value={dishName}
+            onChange={(e) => setdishName(e.target.value)}
+            className="form-control"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="price">Price:</label>
+          <input
+            type="number"
+            id="price"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+            className="form-control"
+            required
+            min="0"
+            max="2000"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="weight">Weight:</label>
+          <input
+            type="number"
+            id="weight"
+            value={weight}
+            onChange={(e) => setWeight(e.target.value)}
+            className="form-control"
+            required
+            min="0"
+            max="999.99"
+            step="0.01"
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Add Dish
+        </button>
+      </form>
     </div>
-    
   );
 };
 
--- src/Components/EditDish.js
+++ src/Components/EditDish.js
@@ -1,50 +1,51 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-
 const EditDish = () => {
   const [dishName, setdishName] = useState('');
-  const [price, setPrice] = useState();
-  const [weight, setWeight] = useState();
-  const [editDish,setEditDish]=useState();
-  const [Id,setId]=useState();
+  const [price, setPrice] = useState('');
+  const [weight, setWeight] = useState('');
+  const [editDish, setEditDish] = useState(null);
+  const [Id, setId] = useState('');
   const navigate = useNavigate();
+
   useEffect(() => {
-    // Extract the dish item from local storage
-      const temp =  JSON.parse(localStorage.getItem('Edit'));
-      setEditDish(temp);
-      setdishName(temp.dishName);
-      setPrice(temp.price);
-      setWeight(temp.weight);
-      setId(temp.id);
-      console.log("Outside getData",temp);
-      console.log(temp.dishName);
-    
+    const temp = JSON.parse(localStorage.getItem('Edit'));
+    setEditDish(temp);
+    setdishName(temp?.dishName || '');
+    setPrice(temp?.price || '');
+    setWeight(temp?.weight || '');
+    setId(temp?.id || '');
   }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    // Create a new dish object
+    // if (!dishName || !price || !weight) {
+    //   alert('Please fill in all fields.');
+    //   return;
+    // }
+
+    // if (parseFloat(price) < 0 || parseFloat(price) > 2000) {
+    //   alert('Price must be in the range of 0 to 2000.');
+    //   return;
+    // }
+
+    // if (parseFloat(weight) < 0 || parseFloat(weight) > 999.99) {
+    //   alert('Weight must be in the range of 0 to 999.99.');
+    //   return;
+    // }
+
     const newDish = {
       Id,
       dishName,
-      price,
-      weight
+      price: parseFloat(price),
+      weight: parseFloat(weight),
     };
 
     try {
-      // Send the new dish data to the backend API
-      console.log(newDish);
-      newDish.Id=parseInt(Id);
-      newDish.dishName=dishName;
-      newDish.price=parseInt(price);
-      newDish.weight=parseInt(weight);
-      console.log("Before Axios Post",newDish);
-      const response = await axios.put(`https://webappnewcreated.azurewebsites.net/api/v1/update/${Id}`, newDish);
-
-      // Reset the form fields
+      await axios.put(`https://webappnewcreated.azurewebsites.net/api/v1/update/${Id}`, newDish);
       setdishName('');
       setPrice('');
       setWeight('');
@@ -53,59 +54,62 @@ const EditDish = () => {
       console.error(error);
     }
   };
-  const CancelEdit = () => {
-    navigate("/home");
+
+  const handleCancelEdit = () => {
+    navigate('/home');
   };
 
   return (
-    <>
-       <div className="container my-5">
-       {editDish && <form onSubmit={handleSubmit}>
-      <div className="form-group">
-        <label htmlFor="dishName">Name of Dish:</label>
-        <input
-          type="text"
-          id="dishName"
-          value={dishName}
-          onChange={(e) => setdishName(e.target.value)}
-          className="form-control"
-          required
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="price">Price:</label>
-        <input
-          type="number"
-          id="price"
-          value={price}
-          onChange={(e) => setPrice(e.target.value)}
-          className="form-control"
-          required
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="weight">Weight:</label>
-        <input
-          type="number"
-          id="weight"
-          value={weight}
-          onChange={(e) => setWeight(e.target.value)}
-          className="form-control"
-          required
-        />
-      </div>
-      <button type="submit" className="btn btn-primary">
-        Edit Dish
-      </button>
-      <button className="btn btn-primary" onClick={() => CancelEdit()}>
-          Cancel Edit
-      </button>
-    </form>}
-       </div>
-       
-       
-    </>
-    
+    <div className="container my-5">
+      {editDish && (
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="dishName">Name of Dish:</label>
+            <input
+              type="text"
+              id="dishName"
+              value={dishName}
+              onChange={(e) => setdishName(e.target.value)}
+              className="form-control"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="price">Price:</label>
+            <input
+              type="number"
+              id="price"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+              className="form-control"
+              required
+              min="0"
+              max="2000"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="weight">Weight:</label>
+            <input
+              type="number"
+              id="weight"
+              value={weight}
+              onChange={(e) => setWeight(e.target.value)}
+              className="form-control"
+              required
+              min="0"
+              max="999.99"
+              step="0.01"
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Edit Dish
+          </button>
+          <button className="btn btn-primary" onClick={handleCancelEdit}>
+            Cancel Edit
+          </button>
+        </form>
+      )}
+    </div>
   );
 };
 
